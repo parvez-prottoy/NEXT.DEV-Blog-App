e3c34bca5552c3b377c50c3c8d051b514497d566
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-09-28 20:46:51 +0600

    added favicon and some tailwind configaration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      mobile: "480px",
+      tablet: "768px",
+      laptop: "1024px",
+      desktop: "1280px",
+    },
+    colors: {
+      primary: "#242424",
+      secondary: "#6B6B6B",
+      background: "#F2F2F2",
+    },
+    extend: {
+      container: {
+        center: true, // center horizontally
+      },
+    },
   },
   plugins: [],
 };
