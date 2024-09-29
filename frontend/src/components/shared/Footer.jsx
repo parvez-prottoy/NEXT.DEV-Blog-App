5f8d394f11455f418c14cca1c3c40c9274d2a9fd
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-09-29 12:07:42 +0600

    added footer component

--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -1,5 +1,12 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  const date = new Date().getFullYear();
+  return (
+    <footer className="mt-6">
+      <div className="py-6 text-sm text-center text-secondary bg-background">
+        © {date} Parvez Ahmed Prottoy. All rights reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
